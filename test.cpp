068eb4067c08ef47848780334b77f746d59df408
whysosaxy <40880787+whysosaxy@users.noreply.github.com>
2021-06-27 14:06:52 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,10 +3,27 @@ class matrix
 {
     int ROWS{50}, COLS{50};
     std::vector<std::vector<int>> table{ROWS, std::vector<int>{COLS, 0}};
-    public:
+    
+public:
+    void printer()
+    {
+        std::vector<std::vector<int>>::iterator row;
+        std::vector<int>::iterator col;
+        
+        for(row = begin(table); row != end(table); row++)
+        {
+            for(col = row->begin(); col != row->end(); col++)
+            {
+                std::cout << *col << ' ';
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
     void printSize()
     {
-        std::cout << "the size of matrix is : " << sizeof(table) << std::endl;
+        std::cout << "the size of matrix is : " << table.size() << std::endl;
     }
 };
 
@@ -14,4 +31,5 @@ int main()
 {
     matrix obj;
     obj.printSize();
+    obj.printer();
 }
